aa9e456f080a7cadedc36d9cc532675781e8b560
Raphael Uziel <raphaeluziel@hotmail.com>
2020-01-03 15:59:18 -0500

    add many photons

--- scripts/index.js
+++ scripts/index.js
@@ -1,69 +1,90 @@
 document.addEventListener('DOMContentLoaded', function(){
   var config = {
-      type: Phaser.AUTO,
-      width: 800,
-      height: 600,
-      physics: {
-          default: 'arcade',
-          arcade: {
-              gravity: { y: 300 },
-              debug: false
-          }
-      },
-      scene: {
-          preload: preload,
-          create: create,
-          update: update
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 300 },
+          debug: false
       }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
   };
 
   var photon;
 
   var game = new Phaser.Game(config);
 
-  let vx = 60;
-  let vy = -100;
+  let vx = -200;
+  let vy = 120;
   let hit = false;
 
   function preload ()
   {
-      this.load.image('metal', 'images/metal.png');
-      this.load.spritesheet('photon', 'images/photons.png', { frameWidth: 248, frameHeight: 112});
+    this.load.image('metal', 'images/metal.png');
+    this.load.spritesheet('photon', 'images/photons.png', { frameWidth: 248, frameHeight: 112});
   }
 
   function create ()
   {
-      // My photons
-      photon = this.physics.add.sprite(650, 100, 'photon');
-      photon.setGravityY(-300);
-      photon.setScale(0.2);
-      photon.angle = -30;
+    // The metal
+    metal = this.physics.add.staticGroup();
+    metal.create(200, 400, 'metal').setScale(0.5).refreshBody();
 
-      metal = this.physics.add.staticGroup();
-      metal.create(200, 400, 'metal').setScale(0.5).refreshBody();
+    // My photons
+    photon = this.physics.add.sprite(650, 100, 'photon');
+    photon.setGravityY(-300);
+    photon.setScale(0.2);
+    photon.angle = -30;
 
-      // photon
-      this.anims.create({
-          key: 'hell',
-          frames: [ { key: 'photon', frame: 5 } ],
-          frameRate: 20
-      });
+    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+    var photons = this.physics.add.group({
+        key: 'photon',
+        repeat: 8,
+        setXY: { x: 450, y: 100, stepX: 30 },
+    });
 
-      //  Input Events
-      cursors = this.input.keyboard.createCursorKeys();
+    photons.setVelocityX(vx);
+    photons.setVelocityY(vy);
+
+    photons.children.iterate(function (child) {
+
+        //  Give each photon a different ???????
+        child.setScale(0.2);
+        child.angle = -30;
+        child.setGravityY(-300);
+
+
+    });
+
+    // photon
+    this.anims.create({
+        key: 'hell',
+        frames: [ { key: 'photon', frame: 0 } ],
+        frameRate: 20
+    });
+
+    //  Input Events
+    cursors = this.input.keyboard.createCursorKeys();
 
   }
 
   function update ()
   {
-      if (photon.x < 200 && !hit){
-        hit = true;
-        vx *= -1;
-        photon.angle *= -1;
-      }
-      photon.setVelocityY(vx);
-      photon.setVelocityX(vy);
-      photon.anims.play('hell');
+    if (photon.x < 215 && !hit){
+      hit = true;
+      vx *= -1;
+      photon.angle *= -1;
+    }
+    photon.setVelocityX(vx);
+    photon.setVelocityY(vy);
+    photon.anims.play('hell');
   }
 
 });
