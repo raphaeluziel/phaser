d6c3b89c23ae9c1008125bf7dc3587f96a7c57dc
unknown <raphaeluziel@hotmail.com>
2020-01-09 09:38:09 -0500

    getting somewhere

--- scripts/index.js
+++ scripts/index.js
@@ -11,13 +11,14 @@ document.addEventListener('DOMContentLoaded', function(){
   };
 
   var photon;
+  var photons;
   var flashlight;
   var metal;
 
   var game = new Phaser.Game(config);
 
-  let vx = -16;
-  let vy = 6;
+  let vx = -8;
+  let vy = 3;
   let hit = false;
 
   function preload ()
@@ -29,27 +30,59 @@ document.addEventListener('DOMContentLoaded', function(){
 
   function create ()
   {
-    metal = this.add.image(400, 300, 'metal');
+    metal = this.add.image(100, 298, 'metal');
     metal.setScale(0.4);
-    metal.x = 100;
 
-    flashlight = this.add.image(400, 300, 'flashlight');
+    flashlight = this.add.image(700, 80, 'flashlight');
     flashlight.setScale(0.2);
-    flashlight.y = 100;
-    flashlight.x = 700;
-    flashlight.angle = 3;
+    flashlight.angle = 9;
 
-    photon = this.add.image(400, 300, 'photon');
+    photon = this.add.image(700, 85, 'photon');
     photon.setScale(0.2);
-    photon.y = 93;
+    photon.y = 73;
     photon.x = 700;
-    photon.angle = -30;
+    photon.angle = -20;
+    photon.setFrame(4);
+
+    photons = this.add.group({
+        key: 'photon',
+        repeat: 3,
+        setXY: { x: 450, y: 100, stepX: 40 },
+    });
+    //photons.setScale(0.2);
+    photons.y = 73;
+    photons.x = 700;
+    photons.angle = -20;
+    //photons.setFrame(4);
+
+    photons.children.iterate(function (child) {
+        child.setScale(0.2);
+        child.angle = -20;
+        child.setFrame(4);
+        child.y = 73;
+        //child.x = 400;
+    });
+
   }
 
-  function update ()
+  function update (t)
   {
-    //photon.x += vx;
-    //photon.y += vy;
+    /*
+    if (photons.x < 120) {
+      vx *= -1;
+      photons.angle *= -1;
+    }
+    photons.x += vx;
+    photons.y += vy;
+    */
+
+
+    if (photon.x < 120) {
+      vx *= -1;
+      photon.angle *= -1;
+    }
+    photon.x += vx;
+    photon.y += vy;
   }
 
 });
